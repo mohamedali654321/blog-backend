ec97163bee60597b04d766d9f9b1e32f1f1139b2
mohamed <mmmmali654321@gmail.com>
2021-06-06 13:24:27 +0000

    first commit

--- api/channel/controllers/channel.js
+++ api/channel/controllers/channel.js
@@ -6,13 +6,12 @@
  */
  const { sanitizeEntity } = require('strapi-utils');
 module.exports = {
-
     async findOne(ctx) {
         const { slug } = ctx.params;
     
-        const entity = await strapi.services.category.findOne({ slug });
-       
-        return sanitizeEntity(entity, { model: strapi.models.category });
+        const entity = await strapi.services.channel.findOne({ slug });
+        
+        return sanitizeEntity(entity, { model: strapi.models.channel });
       },
 
 };
--- api/issue/controllers/issue.js
+++ api/issue/controllers/issue.js
@@ -6,13 +6,12 @@
  */
  const { sanitizeEntity } = require('strapi-utils');
 module.exports = {
-
     async findOne(ctx) {
         const { slug } = ctx.params;
     
-        const entity = await strapi.services.category.findOne({ slug });
+        const entity = await strapi.services.issue.findOne({ slug });
        
-        return sanitizeEntity(entity, { model: strapi.models.category });
+        return sanitizeEntity(entity, { model: strapi.models.issue });
       },
 
 };
